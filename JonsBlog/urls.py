401d5e21ecd073e66c2445593c3e5eef03af088f
Jon Eastin <picardncce@gmail.com>
2022-10-31 16:50:11 -0700

    added views for browsing of blog pages.

--- JonsBlog/urls.py
+++ JonsBlog/urls.py
@@ -5,6 +5,6 @@ from .views import HomeView, ArticleDetailView
 # Using class based url
 urlpatterns = [
     path('', HomeView.as_view(), name="home"),
-    path('/article/<int:pk>', ArticleDetailView.as_view(),
+    path('article/<int:pk>', ArticleDetailView.as_view(),
          name="article-detail"),
 ]
