d25425bb15996d7b492cd706d9092f121c2c5569
Jon Eastin <picardncce@gmail.com>
2022-10-31 20:05:44 -0700

    implemented basic styling for the addpost view

--- JonsBlog/views.py
+++ JonsBlog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from .forms import PostForm
 
 
 # Utilizing Python classes to make the views
@@ -16,5 +17,5 @@ class ArticleDetailView(DetailView):
 
 class AddPostView(CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'add_post.html'
-    fields = '__all__'
