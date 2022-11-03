f2615aa6ed2051ca2c51eb500492a06aeba694cb
Jon Eastin <picardncce@gmail.com>
2022-11-03 09:54:24 -0700

    created update profile function and view

--- members/views.py
+++ members/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.forms import UserChangeForm
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -9,3 +10,12 @@ class UserRegisterView(generic.CreateView):
     form_class = SignupForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
+
+
+class UserEditView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('home')
+
+    def get_object(self):
+        return self.request.user
