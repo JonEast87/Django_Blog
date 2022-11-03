693bc994e658de5708b513043cdaeaceeae11ce0
Jon Eastin <picardncce@gmail.com>
2022-11-03 10:13:47 -0700

    added bootstrap to the user edit page

--- members/forms.py
+++ members/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.contrib.auth.models import User
 
 
@@ -19,3 +19,22 @@ class SignupForm(UserCreationForm):
         self.fields['username'].widget.attrs.update({'class': 'form-control'})
         self.fields['password1'].widget.attrs.update({'class': 'form-control'})
         self.fields['password2'].widget.attrs.update({'class': 'form-control'})
+
+
+class EditProfileForm(UserChangeForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'class': 'form-control'}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'class': 'form-control'}))
+    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
+        'class': 'form-control'}))
+
+    # is_staff = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={
+    #     'class': 'form-check'}))
+    # is_superuser = forms.CharField(max_length=100, widget=forms.CheckboxInput(attrs={
+    #     'class': 'form-check'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password')
