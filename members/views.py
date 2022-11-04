bc1e7a13eadc8d6b1105139d73f4549cff7e17ca
Jon Eastin <picardncce@gmail.com>
2022-11-04 12:19:43 -0700

    added custom password changing view

--- members/views.py
+++ members/views.py
@@ -1,7 +1,18 @@
+from django.contrib.auth.views import PasswordChangeView
+from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 
-from .forms import SignupForm, EditProfileForm
+from .forms import SignupForm, EditProfileForm, PasswordUpdateForm
+
+
+class PasswordsChangeView(PasswordChangeView):
+    form_class = PasswordUpdateForm
+    success_url = reverse_lazy('password_success')
+
+
+def password_success(request):
+    return render(request, 'registration/password_success.html', {})
 
 
 # Create your views here.
