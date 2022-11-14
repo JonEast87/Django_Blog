8c8d78c5a891877f5837b39c200e8f14cb0c12ea
Jon Eastin <picardncce@gmail.com>
2022-11-13 19:18:40 -0700

    reworked add_post and update_post for bulma

--- JonsBlog/views.py
+++ JonsBlog/views.py
@@ -79,12 +79,12 @@ class AddPostView(CreateView):
     model = Post
     template_name = 'add_post.html'
 
-    def get_context_data(self, *args, **kwargs):
-        cat_menu = Category.objects.all()
-        context = super(AddPostView, self).get_context_data(*args, **kwargs)
-
-        context["cat_menu"] = cat_menu
-        return context
+    # def get_context_data(self, *args, **kwargs):
+    #     cat_menu = Category.objects.all()
+    #     context = super(AddPostView, self).get_context_data(*args, **kwargs)
+    #
+    #     context["cat_menu"] = cat_menu
+    #     return context
 
     def form_valid(self, form):
         # saved user information to make it available for later usage
