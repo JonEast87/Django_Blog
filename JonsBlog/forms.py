cc4d37efe1e447c9e6f66db1e07ad9e7f7a48b60
Jon Eastin <picardncce@gmail.com>
2022-11-13 19:18:40 -0700

    reworked add_post and update_post for bulma

--- JonsBlog/forms.py
+++ JonsBlog/forms.py
@@ -4,39 +4,102 @@ from .models import Post, Category, Comment
 
 # Retrieves key-value pair
 choices = Category.objects.all().values_list('name', 'name')
-
+# Creating empty list for the categories to be stored in
 choice_list = []
-
-# adding each category into our Python list to send back to the client
+# Pushing each Category into our Python List to send back to the client
 for item in choices:
     choice_list.append(item)
 
 
 class PostForm(forms.ModelForm):
+
+    title = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={
+                'placeholder': 'Title',
+                'class': 'input is-success'
+            }
+        )
+    )
+
+    title_tag = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={
+                'placeholder': 'Sub title/Snippet',
+                'class': 'input is-info'
+            }
+        )
+    )
+
+    category = forms.ChoiceField(
+        required=True,
+        choices=choice_list,
+        widget=forms.widgets.Select(
+            attrs={
+                'class': 'select is-rounded'
+            }
+        )
+    )
+
+    image = forms.FileField(
+        required=False,
+        widget=forms.widgets.FileInput(
+            attrs={
+                'class': 'file-input',
+                'type': 'file',
+            }
+        )
+    )
+
+    body = forms.CharField(
+        required=True,
+        widget=forms.widgets.Textarea(
+            attrs={
+                'class': 'textarea is-primary is-rounded',
+                'placeholder': 'Blog entry...'
+            }
+        )
+    )
+
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'header_image', 'category', 'body')
-
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            # 'images': forms.ImageField(attrs={'class': 'form-control'}),
-            # 'snippet': forms.TextInput(attrs={'class': 'form-control'}),
-            'category': forms.Select(choices=choice_list, attrs={'class': 'select'}),
-            'body': forms.Textarea(attrs={'class': 'text-area'}),
-        }
+        fields = ('title', 'title_tag', 'category', 'image', 'body')
 
 
 class UpdateForm(forms.ModelForm):
+
+    title = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={
+                'class': 'input is-info'
+            }
+        )
+    )
+
+    title_tag = forms.CharField(
+        required=True,
+        widget=forms.widgets.TextInput(
+            attrs={
+                'class': 'input is-info'
+            }
+        )
+    )
+
+    body = forms.CharField(
+        required=True,
+        widget=forms.widgets.Textarea(
+            attrs={
+                'class': 'textarea is-info',
+            }
+        )
+    )
+
     class Meta:
         model = Post
-        fields = ('title', 'snippet', 'body')
-
-        widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'snippet': forms.TextInput(attrs={'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'class': 'form-control'}),
-        }
+        fields = ('title', 'title_tag', 'body')
 
 
 class CommentForm(forms.ModelForm):
