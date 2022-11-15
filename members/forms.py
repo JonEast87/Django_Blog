d11625cb3d2ff013ab7d00be3e83fb2f4dbf7e74
Jon Eastin <picardncce@gmail.com>
2022-11-14 23:57:42 -0700

    more changes made to both members and JonsBlog for bulma changes.

--- members/forms.py
+++ members/forms.py
@@ -1,15 +1,49 @@
 from JonsBlog.models import Profile
 from django import forms
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm, PasswordChangeForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm, PasswordChangeForm
 from django.contrib.auth.models import User
 
 
+class LoginForm(AuthenticationForm):
+    #
+    # username = forms.CharField(
+    #     required=True,
+    #     widget=forms.widgets.TextInput(
+    #         attrs={
+    #             'class': 'input is-info',
+    #             'type': 'text',
+    #         }
+    #     )
+    # )
+    #
+    # password = forms.CharField(
+    #     required=True,
+    #     widget=forms.widgets.TextInput(
+    #         attrs={
+    #             'class': 'input is-info',
+    #             'type': 'password',
+    #         }
+    #     )
+    # )
+
+    class Meta:
+        model = User
+        # fields = ('username', 'password')
+
+    def __int__(self, *args, **kwargs):
+        super(LoginForm, self).__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({'class': 'input is-info'})
+        self.fields['password'].widget.attrs.update({'class': 'input is-info'})
+
+
 class SignupForm(UserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'input is-info'}))
+
     first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
-        'class': 'form-control'}))
+        'class': 'input is-info'}))
+
     last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
-        'class': 'form-control'}))
+        'class': 'input is-info'}))
 
     class Meta:
         model = User
@@ -17,47 +51,169 @@ class SignupForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super(SignupForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs.update({'class': 'form-control'})
-        self.fields['password1'].widget.attrs.update({'class': 'form-control'})
-        self.fields['password2'].widget.attrs.update({'class': 'form-control'})
+        self.fields['username'].widget.attrs.update({'class': 'input is-info'})
+        self.fields['password1'].widget.attrs.update({'class': 'input is-info'})
+        self.fields['password2'].widget.attrs.update({'class': 'input is-info'})
 
 
 class CreateProfileForm(forms.ModelForm):
+    bio = forms.CharField(
+        max_length=255,
+        required=True,
+        widget=forms.widgets.Textarea(
+            attrs={
+                'class': 'textarea is-info',
+                'placeholder': 'Bio entry, max of 255 characters...'
+            }
+        )
+    )
+
+    profile_image = forms.ImageField(
+        required=True,
+        widget=forms.widgets.FileInput(
+            attrs={
+                'class': 'file-input',
+                'type': 'file',
+            }
+        )
+    )
+
+    github = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.widgets.TextInput(
+            attrs={
+                'class': 'input is-info'
+            }
+        )
+    )
+
+    instagram = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.widgets.TextInput(
+            attrs={
+                'class': 'input is-info'
+            }
+        )
+    )
+
+    linkedin = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.widgets.TextInput(
+            attrs={
+                'class': 'input is-info'
+            }
+        )
+    )
+
+    twitter = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.widgets.TextInput(
+            attrs={
+                'class': 'input is-info'
+            }
+        )
+    )
+
     class Meta:
         model = Profile
         fields = ('bio', 'profile_image', 'github', 'instagram', 'linkedin', 'twitter')
-
-        widgets = {
-            'bio': forms.Textarea(attrs={'class': 'form-control'}),
-            'github': forms.TextInput(attrs={'class': 'form-control', }),
-            'instagram': forms.TextInput(attrs={'class': 'form-control'}),
-            'linkedin': forms.TextInput(attrs={'class': 'form-control'}),
-            'twitter': forms.TextInput(attrs={'class': 'form-control'}),
-        }
 
 
 class EditProfileForm(forms.ModelForm):
+    bio = forms.CharField(
+        required=False,
+        widget=forms.widgets.Textarea(
+            attrs={
+                'class': 'textarea is-success',
+            }
+        )
+    )
+
+    github = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.widgets.TextInput(
+            attrs={
+                'class': 'input is-success'
+            }
+        )
+    )
+
+    instagram = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.widgets.TextInput(
+            attrs={
+                'class': 'input is-success'
+            }
+        )
+    )
+
+    linkedin = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.widgets.TextInput(
+            attrs={
+                'class': 'input is-success'
+            }
+        )
+    )
+
+    twitter = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.widgets.TextInput(
+            attrs={
+                'class': 'input is-success'
+            }
+        )
+    )
+
     class Meta:
         model = Profile
         fields = ('bio', 'profile_image', 'github', 'instagram', 'linkedin', 'twitter')
 
-        widgets = {
-            'bio': forms.Textarea(attrs={'class': 'form-control'}),
-            'github': forms.TextInput(attrs={'class': 'form-control', }),
-            'instagram': forms.TextInput(attrs={'class': 'form-control'}),
-            'linkedin': forms.TextInput(attrs={'class': 'form-control'}),
-            'twitter': forms.TextInput(attrs={'class': 'form-control'}),
-        }
-
 
 class EditSettingsForm(UserChangeForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
-        'class': 'form-control'}))
-    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
-        'class': 'form-control'}))
-    username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
-        'class': 'form-control'}))
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={
+                'class': 'input is-success',
+                'type': 'email'
+            }
+        )
+    )
+
+    first_name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'input is-success'
+            }
+        )
+    )
+
+    last_name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'input is-success'
+            }
+        )
+    )
+
+    username = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(
+            attrs={
+                'class': 'input is-success'
+            }
+        )
+    )
 
     class Meta:
         model = User
@@ -67,8 +223,10 @@ class EditSettingsForm(UserChangeForm):
 class PasswordUpdateForm(PasswordChangeForm):
     old_password = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
         'class': 'form-control', 'type': 'password'}))
+
     new_password1 = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
         'class': 'form-control', 'type': 'password'}))
+
     new_password2 = forms.CharField(max_length=100, widget=forms.TextInput(attrs={
         'class': 'form-control', 'type': 'password'}))
 
--- members/views.py
+++ members/views.py
@@ -1,23 +1,28 @@
-from JonsBlog.models import Profile
-from django.contrib.auth.views import PasswordChangeView
+from JonsBlog.models import Profile, User
+from django.contrib.auth.views import PasswordChangeView, LoginView
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 from django.views.generic import DetailView, CreateView
 
 from .forms import SignupForm, PasswordUpdateForm, EditSettingsForm, EditProfileForm, \
-    CreateProfileForm
+    CreateProfileForm, LoginForm
 
 
 class CreateProfilePageView(CreateView):
     model = Profile
-    template_name = "registration/create_user_profile.html"
+    template_name = 'registration/create_user_profile.html'
     form_class = CreateProfileForm
 
     def form_valid(self, form):
         # saved user information to make it available for later usage
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+#
+# class LoginPageView(LoginView):
+#     model = User
+#     form_class = LoginForm
 
 
 class ShowProfilePageView(DetailView):
@@ -27,7 +32,7 @@ class ShowProfilePageView(DetailView):
     def get_context_data(self, *args, **kwargs):
         context = super(ShowProfilePageView, self).get_context_data(*args, **kwargs)
         page_user = get_object_or_404(Profile, id=self.kwargs['pk'])
-        context["page_user"] = page_user
+        context['page_user'] = page_user
         return context
 
 
