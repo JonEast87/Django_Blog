036c25e1f0320ed019a8e81b7cdb22913d990c3c
Jon Eastin <picardncce@gmail.com>
2022-11-23 21:29:23 -0700

    may have an extra * in the settings.py causing an status 400

--- JonBlog/settings.py
+++ JonBlog/settings.py
@@ -41,7 +41,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.getenv('SECRET_KEY', 'False') == 'True')
 
-ALLOWED_HOSTS = ['127.0.0.1', '*.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
 # Application definition
 
