a221d38a005b222e2c2cab40fea96e529815645c
Jon Eastin <picardncce@gmail.com>
2022-11-23 20:48:49 -0700

    more heroku work

--- JonBlog/settings.py
+++ JonBlog/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'JonBlog.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('DATABASE_KEY'),
+        'NAME': os.getenv('DATABASE_NAME'),
         'USER': os.getenv('DATABASE_USER'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': os.getenv('DATABASE_HOST'),
